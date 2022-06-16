ffc189824293034072fa0e137e0f56ce25966035
maiduchien23 <93573217+maiduchien23@users.noreply.github.com>
2022-06-16 15:23:38 +0700

    Add files via upload

---
+++ Lab5._0/ConnectionData.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace product
+{
+    public class ConnectionData
+    {
+        public SqlConnection SqlConnection { get; private set; }
+
+        public SqlConnection GetConnection()
+        {
+
+
+            string connectionString = @"Data Source=LAPTOP-21F5N0OT\SQLEXPRESS;Initial Catalog=DbTest;Integrated Security=True";
+
+            SqlConnection = new SqlConnection(connectionString);
+
+            return SqlConnection;
+
+
+        }
+
+        internal SqlConnection GetDatabase()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
